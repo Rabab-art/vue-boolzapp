86bc195fe17f7291707b185d07452f8c117f2573
Rabab-art <bouhamla.rabab@gmail.com>
2021-08-30 19:10:13 +0200

    Inserimento messaggi in chat  con v-for

--- js/script.js
+++ js/script.js
@@ -13,10 +13,52 @@ const app = new Vue({
             {
                 name: 'Michele',
                 avatar: '_1',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2021 - 10:07',
+                        message: 'Hai portato fuori il cane',
+                        status: 'sent',
+
+                    },
+                    {
+                        date: '10/01/2021 - 10:07',
+                        message: 'Ricordati di dagli da mangiare',
+                        status: 'sent',
+                    },
+
+                    {
+                        date: '10/01/2021 - 10:07',
+                        message: 'ok',
+                        status: 'received',
+                    },
+                ],
+
+
             },
             {
                 name: 'Fabio',
                 avatar: '_2',
+                // visible: true,
+                // item: [
+                //     {
+                //         date: '10/01/2021 - 10:07',
+                //         message: 'Ciao, come stai?',
+                //         status: 'sent',
+
+                //     },
+                //     {
+                //         date: '10/01/2021 - 10:07',
+                //         message: 'Bene grazie e tu?',
+                //         status: 'received',
+                //     },
+
+                //     {
+                //         date: '10/01/2021 - 10:07',
+                //         message: 'ok',
+                //         status: 'sent',
+                //     },
+                // ]
             },
             {
                 name: 'Samuele',
@@ -27,6 +69,7 @@ const app = new Vue({
                 avatar: '_4',
             },
         ],
+
     },
     methods: {},
 })
